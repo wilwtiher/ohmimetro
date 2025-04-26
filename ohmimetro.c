5d58c672d019d7abed6bfe33737532fb66603669
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-04-26 13:10:26 -0300

    programa do professor Wilton

--- ohmimetro.c
+++ ohmimetro.c
@@ -36,4 +36,68 @@ int main()
   gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
   // Aqui termina o trecho para modo BOOTSEL com botão B
 
+  
+  gpio_init(Botao_A);
+  gpio_set_dir(Botao_A, GPIO_IN);
+  gpio_pull_up(Botao_A);
+
+  // I2C Initialisation. Using it at 400Khz.
+  i2c_init(I2C_PORT, 400 * 1000);
+
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+  gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+  gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+  ssd1306_t ssd;                                                // Inicializa a estrutura do display
+  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+  ssd1306_config(&ssd);                                         // Configura o display
+  ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+
+  // Limpa o display. O display inicia com todos os pixels apagados.
+  ssd1306_fill(&ssd, false);
+  ssd1306_send_data(&ssd);
+
+  adc_init();
+  adc_gpio_init(ADC_PIN); // GPIO 28 como entrada analógica
+
+  float tensao;
+  char str_x[5]; // Buffer para armazenar a string
+  char str_y[5]; // Buffer para armazenar a string
+
+  bool cor = true;
+  while (true)
+  {
+    adc_select_input(2); // Seleciona o ADC para eixo X. O pino 28 como entrada analógica
+
+    float soma = 0.0f;
+    for (int i = 0; i < 500; i++)
+    {
+      soma += adc_read();
+      sleep_ms(1);
+    }
+    float media = soma / 500.0f;
+
+      // Fórmula simplificada: R_x = R_conhecido * ADC_encontrado /(ADC_RESOLUTION - adc_encontrado)
+      R_x = (R_conhecido * media) / (ADC_RESOLUTION - media);
+
+    sprintf(str_x, "%1.0f", media); // Converte o inteiro em string
+    sprintf(str_y, "%1.0f", R_x);   // Converte o float em string
+
+    // cor = !cor;
+    //  Atualiza o conteúdo do display com animações
+    ssd1306_fill(&ssd, !cor);                          // Limpa o display
+    ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
+    ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
+    ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
+    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
+    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
+    ssd1306_draw_string(&ssd, "  Ohmimetro", 10, 28);  // Desenha uma string
+    ssd1306_draw_string(&ssd, "ADC", 13, 41);          // Desenha uma string
+    ssd1306_draw_string(&ssd, "Resisten.", 50, 41);    // Desenha uma string
+    ssd1306_line(&ssd, 44, 37, 44, 60, cor);           // Desenha uma linha vertical
+    ssd1306_draw_string(&ssd, str_x, 8, 52);           // Desenha uma string
+    ssd1306_draw_string(&ssd, str_y, 59, 52);          // Desenha uma string
+    ssd1306_send_data(&ssd);                           // Atualiza o display
+    sleep_ms(700);
+  }
 }
