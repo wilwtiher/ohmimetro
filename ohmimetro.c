2ab9a1d86b5d44e50c134a87a41363e89832fde9
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-04-26 13:38:20 -0300

    adicionado codigo de cores

--- ohmimetro.c
+++ ohmimetro.c
@@ -9,13 +9,25 @@
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
-#define ADC_PIN 28 // GPIO para o voltímetro
+#define ADC_PIN 28 // GPIO para o ohmimetro
 #define Botao_A 5  // GPIO para botão A
 
 int R_conhecido = 10000;   // Resistor de 10k ohm
 float R_x = 0.0;           // Resistor desconhecido
 float ADC_VREF = 3.31;     // Tensão de referência do ADC
 int ADC_RESOLUTION = 4095; // Resolução do ADC (12 bits)
+const char* cores[] = { // Variavel para código de cores (5 caracteres cada)
+    "Preto",   // 0
+    "Marro",  // 1
+    "Verme",// 2
+    "Laran", // 3
+    "Amare", // 4
+    "Verde",   // 5
+    "Azul.",    // 6
+    "Viole", // 7
+    "Cinza",   // 8
+    "Branc"   // 9
+};
 
 // Trecho para modo BOOTSEL com botão B
 #include "pico/bootrom.h"
@@ -80,6 +92,21 @@ int main()
       // Fórmula simplificada: R_x = R_conhecido * ADC_encontrado /(ADC_RESOLUTION - adc_encontrado)
       R_x = (R_conhecido * media) / (ADC_RESOLUTION - media);
 
+      // Conta para saber codigo de cor
+      int ordem = 0;
+      float resistencia = R_x;
+      while (resistencia >= 100) {
+          resistencia /= 10;
+          ordem++;
+      }
+      while (resistencia < 10) {
+          resistencia *= 10;
+          ordem--;
+      }
+  
+      int digito1 = (int)resistencia / 10;
+      int digito2 = (int)resistencia % 10;
+  
     sprintf(str_x, "%1.0f", media); // Converte o inteiro em string
     sprintf(str_y, "%1.0f", R_x);   // Converte o float em string
 
@@ -89,14 +116,23 @@ int main()
     ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
     ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
     ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
-    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
-    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
-    ssd1306_draw_string(&ssd, "  Ohmimetro", 10, 28);  // Desenha uma string
-    ssd1306_draw_string(&ssd, "ADC", 13, 41);          // Desenha uma string
-    ssd1306_draw_string(&ssd, "Resisten.", 50, 41);    // Desenha uma string
+    ssd1306_draw_string(&ssd, "Ohmimetro", 8, 6); // Desenha uma string
+    ssd1306_draw_string(&ssd, "Resistenc.", 8, 16);  // Desenha uma string
+    ssd1306_draw_string(&ssd, str_y, 8, 26);  // Desenha uma string
+    ssd1306_draw_string(&ssd, "Codig. de cor", 8, 36);          // Desenha uma string
+    // codigo de cores
+    ssd1306_draw_string(&ssd, cores[digito1], 8, 46);    // Desenha uma string
+    ssd1306_draw_string(&ssd, ",", 48, 46);    // Desenha uma string
+    ssd1306_draw_string(&ssd, cores[digito2], 54, 46);    // Desenha uma string
+    ssd1306_draw_string(&ssd, ",", 94, 46);    // Desenha uma string
+    ssd1306_draw_string(&ssd, cores[ordem], 8, 56);    // Desenha uma string
+    ssd1306_draw_string(&ssd, ",", 48, 46);    // Desenha uma string
+    ssd1306_draw_string(&ssd, "dourado.", 54, 46);    // Desenha uma string
+    /*
     ssd1306_line(&ssd, 44, 37, 44, 60, cor);           // Desenha uma linha vertical
     ssd1306_draw_string(&ssd, str_x, 8, 52);           // Desenha uma string
     ssd1306_draw_string(&ssd, str_y, 59, 52);          // Desenha uma string
+    */
     ssd1306_send_data(&ssd);                           // Atualiza o display
     sleep_ms(700);
   }
