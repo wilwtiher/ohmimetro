3cc4b958c5697ece473cb86530743d8e3d91b6b9
Wilton Júnior <wiltonjrtablet@gmail.com>
2025-04-26 13:05:21 -0300

    incluicao de bibliotecas

--- ohmimetro.c
+++ ohmimetro.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 #include "hardware/i2c.h"
-
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+#define ADC_PIN 28 // GPIO para o voltímetro
+#define Botao_A 5  // GPIO para botão A
 
 
 
